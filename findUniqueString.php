f0d902e38abde55729b25ef7c585945196f204cf
Jacek Kalasz <jkalasz@protonmail.com>
2023-03-25 21:41:34 +0100

    funny functions

---
+++ findUniqueString.php
@@ -0,0 +1,56 @@
+<?php
+
+function findUniqueString(string $s): int
+{
+    $charCount = [];
+    $length = strlen($s);
+    // Count the occurrence of each character in the string
+    for ($i = 0; $i < $length; $i++) {
+        $char = $s[$i];
+        if (isset($charCount[$char])) {
+            $charCount[$char]++;
+        } else {
+            $charCount[$char] = 1;
+        }
+    }
+    // Find the index of the first unique character
+    for ($i = 0; $i < $length; $i++) {
+        $char = $s[$i];
+        if ($charCount[$char] == 1) {
+            return $i + 1;
+        }
+    }
+    return -1;
+}
+function test():void
+{
+    $data = [
+        'hashthegame' => 3,
+        'statistics'  => 3,
+        'abbbbbb'     => 1,
+        'bbbbbba'     => 7,
+        'bbbabba'     => -1,
+        'bbbabbz'     => 4,
+    ];
+    foreach ($data as $input => $validOutput)
+    {
+        if (findUniqueString($input) !== $validOutput) {
+            throw new Error("That function sucks with input $input and output $validOutput");
+        }
+    }
+    echo ("Test passed, " . count($data) . " test cases checked.\n");
+}
+function testBigString():void
+{
+    $maxRange          = 10^5;
+    $string            = str_repeat('x', $maxRange);
+    $assertEmptySearch = findUniqueString($string) === -1;
+    $string            = substr_replace($string,"z",-1);
+    $assertSearch      = findUniqueString($string) === $maxRange;
+    if (!$assertEmptySearch || !$assertSearch) {
+        throw new Error('That function sucks with big strings');
+    }
+    echo ("Test big numbers passed, \n");
+}
+test();
+testBigString();
---
+++ hashOrder.php
@@ -0,0 +1,50 @@
+<?php
+
+function hashOrder(int $number): string
+{
+    $max = 9999999;
+    $length = 7;
+    $output = str_pad(strval(abs($number - $max)), $length, 0, STR_PAD_LEFT);
+    $even = $odd = '';
+    for ($i = 0; $i < $length; $i++) {
+        if ($i % 2 === 0) {
+            $even .= $output[$i];
+        } else {
+            $odd .= $output[$i];
+        }
+    }
+    return $odd . strrev($even);
+}
+function test()
+{
+    $unique = [];
+
+    echo "Starting test ....".PHP_EOL;
+    $start = microtime(true);
+
+    for ($i=1; $i<=9999999; $i++) {
+        $result = hashOrder($i);
+        var_dump($result);
+
+        if (!preg_match("/^[0-9]{7}$/", $result)) {
+            throw new InvalidArgumentException("Result {$result} does not match regex");
+        }
+
+        if (!empty($unique[$result])) {
+            throw new InvalidArgumentException("Colision detected for key {$i}:{$unique[$result]} and result {$result}");
+        }
+
+        $unique[$result] = $i;
+    }
+
+    $time_elapsed_secs = microtime(true) - $start;
+
+    if ($time_elapsed_secs > 60) {
+        throw new InvalidArgumentException("Could not finish test in 60 seconds");
+    }
+
+    echo "Finished in {$time_elapsed_secs}";
+
+}
+
+test();
